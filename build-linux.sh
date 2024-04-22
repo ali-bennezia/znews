909724c6ea3323102726fa75dc0717d1185e83f5
ali-bennezia <bennezia.ali@gmail.com>
2024-04-22 10:53:43 +0200

    shell function utilities

---
+++ build-linux.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+deleteFileIfExists()
+{
+	DEL_FILE="$1"
+	if [ -f "$DEL_FILE" ]; then
+			DEL_FLAGS="-f"
+		elif [ -d "$DEL_FILE" ]; then
+			DEL_FLAGS="-rf"
+		else return
+	fi
+	rm $DEL_FLAGS "$DEL_FILE"
+}
+
+copyFileIfExists()
+{
+	CP_FROM="$1"
+	CP_TO="$2"
+	if [ -f "$CP_FROM" ]; then
+			CP_FLAGS="-f"
+		elif [ -d "$CP_FROM" ]; then
+			CP_FLAGS="-rf"
+		else return
+	fi
+	cp $CP_FLAGS "$CP_FROM" "$CP_TO"
+}
+
+overwriteFile()
+{
+	OW_FROM="$1"
+	OW_TO="$2"
+	deleteFileIfExists "$OW_TO"
+	copyFileIfExists "$OW_FROM" "$OW_TO"
+}
+
+overwriteFile "$1" "$2"
