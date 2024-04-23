ab968a24b61d8acea7153fa720b217f4ebf5104e
ali-bennezia <bennezia.ali@gmail.com>
2024-04-23 13:32:33 +0200

    source & news model modifications

--- src/backend/src/models/sourceModel.js
+++ src/backend/src/models/sourceModel.js
@@ -2,11 +2,17 @@ const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema(
   {
+    name: { type: String, required: true, unique: true },
     url: { type: String, required: true, unique: true },
+    country: { type: String, required: true },
+
+    sourceType: { type: String, required: true, enum: ["api", "page"] },
     lastChecked: { type: Date, required: true },
     selectors: [{ type: String, required: true }],
-    fetchModes: [{ type: String, required: true }],
-    modifiers: [{ type: String, required: true }],
+    fetchModes: [{ type: String, required: true }], // ['json', 'attrib', 'content']
+    modifiers: [
+      { type: String, required: true }, //["trimBegin", "trimEnd"]
+    ],
   },
   { timestamps: true }
 );
